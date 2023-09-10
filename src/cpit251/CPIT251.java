f46284fa1f385d3e9e3b3498b3d1e19b560195b2
Ghada-alshehri <143977601+Ghada-alshehri@users.noreply.github.com>
2023-09-10 19:22:05 +0300

    Update CPIT251.java

--- src/cpit251/CPIT251.java
+++ src/cpit251/CPIT251.java
@@ -7,8 +7,9 @@ public class CPIT251 {
    
     public static void main(String[] args) {
        System.out.print("Hello I'm marya");
-
        System.out.print("Hello I'm layan");
+       System.out.print("Hello I'm GHADA");
+
 
        System.out.println("Hello I'm Afnan");
     }
