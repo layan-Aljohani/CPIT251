53d564699e085cb8aab3cd90e9bcadcee52d6d01
afnan-farhat <143901435+afnan-farhat@users.noreply.github.com>
2023-09-10 19:22:45 +0300

    Update CPIT251.java

--- src/cpit251/CPIT251.java
+++ src/cpit251/CPIT251.java
@@ -9,8 +9,6 @@ public class CPIT251 {
        System.out.print("Hello I'm marya");
        System.out.print("Hello I'm layan");
        System.out.print("Hello I'm GHADA");
-
-
        System.out.println("Hello I'm Afnan");
     }
     
